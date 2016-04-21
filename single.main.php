0c11c669f2a7f52ae81b0fcd63cbe74059f0e85e
Ariflaw <nurariflaw@gmail.com>
2016-04-21 13:01:56 +0700

    Update

--- single.main.php
+++ single.main.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * This is the main/default page template.
- *
- * For a quick explanation of b2evo 2.0 skins, please start here:
- * {@link http://b2evolution.net/man/skin-development-primer}
- *
- * It is used to display the blog when no specific page template is available to handle the request.
- *
- * @package evoskins
- * @subpackage bootstrap_gallery_skin
- */
+* This is the main/default page template.
+*
+* For a quick explanation of b2evo 2.0 skins, please start here:
+* {@link http://b2evolution.net/man/skin-development-primer}
+*
+* It is used to display the blog when no specific page template is available to handle the request.
+*
+* @package evoskins
+* @subpackage bootstrap_gallery_skin
+*/
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
 if( evo_version_compare( $app_version, '6.4' ) < 0 )
@@ -18,6 +18,7 @@ if( evo_version_compare( $app_version, '6.4' ) < 0 )
 }
 
 global $Skin;
+
 // This is the main template; it may be used to display very different things.
 // Do inits depending on current $disp:
 skin_init( $disp );
@@ -31,116 +32,113 @@ skin_include( '_body_header.inc.php' );
 // ------------------------------- END OF SITE HEADER --------------------------------
 
 ?>
-<div class="container">
-   <main id="main_content"><!-- This is were a link like "Jump to main content" would land -->
-   	<!-- =================================== START OF POST TITLE BAR =================================== -->
-   	<?php
-   		if( $single_Item = & mainlist_get_item() )
-   		{ // Get Item here, because it can be not defined yet, e.g. in Preview mode ?>
-   		<nav class="nav_album">
-   			<h3 class="nav_album_title">
-   				<?php
-   					$single_Item->title( array(
-							'link_type' => 'none',
-							'before'    => '',
-							'after'     => '',
-						) );
-   				?>
-   				<?php
-   					if( $Skin->enabled_status_banner( $single_Item->status ) )
-   					{ // Status banner
-   						$single_Item->format_status( array( 'template' => '<div class="evo_status evo_status__$status$ badge">$status_title$</div>' ) );
-   					}
-   					$single_Item->edit_link( array( // Link to backoffice for editing
-							'before'    => '',
-							'after'     => '',
-							'text'      => get_icon( 'edit' ),
-							'title'     => T_('Edit title/description...'),
-						) );
-   				?>
-   			</h3><!-- .nav_album_title -->
-				<!-- <div class="nav_album_pagination"> -->
-					<a href="<?php $Blog->disp( 'url', 'raw' ) ?>" title="<?php echo format_to_output( T_('All Albums'), 'htmlattr' ); ?>" class="all_albums">All Albums</a>
-	   			<?php
-	   				// ------------------- PREV/NEXT POST LINKS (SINGLE POST MODE) -------------------
-	   				item_prevnext_links( array(
-							'template' 		=> '$prev$$next$',
-							'block_start'	=> '<ul class="nav_posts hidden-xs">',
-							'next_class' 	=> 'next',
-							'next_start'   => '<li class="next">',
-							'next_text'		=> T_( 'Next ' ).'<i class="fa fa-angle-right"></i>',
-							'next_no_item' => '',
-							'next_end'     => '</li>',
-							'prev_class'	=> 'previous',
-							'prev_start'   => '<li class="previous">',
-							'prev_text' 	=> '<i class="fa fa-angle-left"></i>'.T_(' Previous'),
-							'prev_no_item' => '',
-							'prev_end'     => '</li>',
-							'block_end'    => '</ul>',
-						) );
-	   				// ------------------------- END OF PREV/NEXT POST LINKS -------------------------
-	   			?>
-				<!-- </div> -->
-   			<!-- <div class="clearfix"></div> -->
-			</nav><!-- .nav_album -->
-   		<?php
-   		} // ------------------- END OF NAVIGATION BAR FOR ALBUM(POST) -------------------
-   	?>
 
-   	<?php
-   		// ------------------------- MESSAGES GENERATED FROM ACTIONS -------------------------
-   		messages( array(
-				'block_start' => '<div class="row"><div class="col-xs-12 action_messages">',
-				'block_end'   => '</div></div>',
+<main id="main_content"><!-- This is were a link like "Jump to main content" would land -->
+	<div class="container">
+	<!-- =================================== START OF POST TITLE BAR =================================== -->
+		<?php
+		if( $single_Item = & mainlist_get_item() ){
+		// Get Item here, because it can be not defined yet, e.g. in Preview mode ?>
+		<nav class="nav_album">
+			<h3 class="nav_album_title">
+				<?php
+				$single_Item->title( array(
+					'link_type' => 'none',
+					'before'    => '',
+					'after'     => '',
+				) );
+				?>
+				<?php
+				if( $Skin->enabled_status_banner( $single_Item->status ) ) { // Status banner
+					$single_Item->format_status( array( 'template' => '<div class="evo_status evo_status__$status$ badge">$status_title$</div>' ) );
+				}
+				$single_Item->edit_link( array( // Link to backoffice for editing
+					'before'    => '',
+					'after'     => '',
+					'text'      => get_icon( 'edit' ),
+					'title'     => T_('Edit title/description...'),
+				) );
+				?>
+			</h3><!-- .nav_album_title -->
+			<a href="<?php $Blog->disp( 'url', 'raw' ) ?>" title="<?php echo format_to_output( T_('All Albums'), 'htmlattr' ); ?>" class="all_albums">All Albums</a>
+			<?php
+			// ------------------- PREV/NEXT POST LINKS (SINGLE POST MODE) -------------------
+			item_prevnext_links( array(
+				'template' 		=> '$prev$$next$',
+				'block_start'	=> '<ul class="nav_posts hidden-xs">',
+				'next_class' 	=> 'next',
+				'next_start'   => '<li class="next">',
+				'next_text'		=> T_( 'Next ' ).'<i class="fa fa-angle-right"></i>',
+				'next_no_item' => '',
+				'next_end'     => '</li>',
+				'prev_class'	=> 'previous',
+				'prev_start'   => '<li class="previous">',
+				'prev_text' 	=> '<i class="fa fa-angle-left"></i>'.T_(' Previous'),
+				'prev_no_item' => '',
+				'prev_end'     => '</li>',
+				'block_end'    => '</ul>',
 			) );
-   		// --------------------------------- END OF MESSAGES ---------------------------------
-   	?>
+			// ------------------------- END OF PREV/NEXT POST LINKS -------------------------
+			?>
+		</nav><!-- .nav_album -->
+		<?php
+		} // ------------------- END OF NAVIGATION BAR FOR ALBUM(POST) -------------------
+		?>
 
-   	<article>
-	   	<?php $Item->locale_temp_switch(); // Temporarily switch to post locale (useful for multilingual blogs) ?>
-	   	<div class="single__post_images">
-	   		<?php
-	   			// Display images that are linked to this post:
-	   			$Item->images( array(
-						'before'              => '',
-						'before_image'        => '<figure class="single-image col-xl-4 col-lg-4 col-md-6 col-sm-12">',
-						'before_image_legend' => '<figcaption class="evo_image_legend">',
-						'after_image_legend'  => '</figcaption>',
-						'after_image'         => '</figure>',
-						'after'               => '<div class="clear"></div>',
-						'image_size'          => $Skin->get_setting( 'single_thumb_size' ),
-						'image_align'         => 'middle',
-						'image_class'         => 'img-responsive',
-						'before_gallery'      => '<div class="evo_post_gallery">',
-						'after_gallery'       => '</div>',
-						'gallery_table_start' => '',
-						'gallery_table_end'   => '',
-						'gallery_row_start'   => '',
-						'gallery_row_end'     => '',
-						'gallery_cell_start'  => '<div class="evo_post_gallery__image">',
-						'gallery_cell_end'    => '</div>',
-					) );
-	   		?>
-	   	</div>
+		<?php
+		// ------------------------- MESSAGES GENERATED FROM ACTIONS -------------------------
+		messages( array(
+			'block_start' => '<div class="row"><div class="col-xs-12 action_messages">',
+			'block_end'   => '</div></div>',
+		) );
+		// --------------------------------- END OF MESSAGES ---------------------------------
+		?>
 
-	   	<div class="evo_post_content">
-	   		<div class="evo_details">
-	   			<div class="evo_container evo_container__item_single">
-	   				<?php
-	   				// ------------------------- "Item Single" CONTAINER EMBEDDED HERE --------------------------
-	   				// WARNING: EXPERIMENTAL -- NOT RECOMMENDED FOR PRODUCTION -- MAY CHANGE DRAMATICALLY BEFORE RELEASE.
-	   				// Display container contents:
-	   				skin_container( /* TRANS: Widget container name */ NT_('Item Single'), array(
-	   					'widget_context'    => 'item',	// Signal that we are displaying within an Item
-	   					// The following (optional) params will be used as defaults for widgets included in this container:
-	   					// This will enclose each widget in a block:
-	   					'block_start'       => '<div class="$wi_class$">',
-	   					'block_end'         => '</div>',
-	   					// This will enclose the title of each widget:
-	   					'block_title_start' => '<h3>',
-	   					'block_title_end'   => '</h3>',
-	   					// Params for skin file "_item_content.inc.php"
-	   					'widget_item_content_params' => array(
+		<article>
+			<?php $Item->locale_temp_switch(); // Temporarily switch to post locale (useful for multilingual blogs) ?>
+			<div class="single__post_images">
+				<?php
+				// Display images that are linked to this post:
+				$Item->images( array(
+					'before'              => '',
+					'before_image'        => '<figure class="single-image col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-6">',
+					'before_image_legend' => '<figcaption class="evo_image_legend">',
+					'after_image_legend'  => '</figcaption>',
+					'after_image'         => '</figure>',
+					'after'               => '<div class="clear"></div>',
+					'image_size'          => $Skin->get_setting( 'single_thumb_size' ),
+					'image_align'         => 'middle',
+					'image_class'         => 'img-responsive',
+					'before_gallery'      => '<div class="evo_post_gallery">',
+					'after_gallery'       => '</div>',
+					'gallery_table_start' => '',
+					'gallery_table_end'   => '',
+					'gallery_row_start'   => '',
+					'gallery_row_end'     => '',
+					'gallery_cell_start'  => '<div class="evo_post_gallery__image">',
+					'gallery_cell_end'    => '</div>',
+				) );
+				?>
+			</div>
+
+			<div class="evo_post_content">
+				<div class="evo_details">
+					<div class="evo_container evo_container__item_single">
+						<?php
+						// ------------------------- "Item Single" CONTAINER EMBEDDED HERE --------------------------
+						// WARNING: EXPERIMENTAL -- NOT RECOMMENDED FOR PRODUCTION -- MAY CHANGE DRAMATICALLY BEFORE RELEASE.
+						// Display container contents:
+						skin_container( /* TRANS: Widget container name */ NT_('Item Single'), array(
+							'widget_context'    => 'item',	// Signal that we are displaying within an Item
+							// The following (optional) params will be used as defaults for widgets included in this container:
+							// This will enclose each widget in a block:
+							'block_start'       => '<div class="$wi_class$">',
+							'block_end'         => '</div>',
+							// This will enclose the title of each widget:
+							'block_title_start' => '<h3>',
+							'block_title_end'   => '</h3>',
+							// Params for skin file "_item_content.inc.php"
+							'widget_item_content_params' => array(
 								'feature_block'          => false,
 								'item_class'             => 'evo_post',
 								'item_type_class'        => 'evo_post__ptyp_',
@@ -149,70 +147,70 @@ skin_include( '_body_header.inc.php' );
 								'image_size'             => '', // Do not display images in content block - Image is handled separately
 								'url_link_text_template' => '', // link will be displayed (except player if podcast)
 							),
-	   				) );
-	   				// ----------------------------- END OF "Item Single" CONTAINER -----------------------------
-	   				?>
-	   			</div>
+						) );
+						// ----------------------------- END OF "Item Single" CONTAINER -----------------------------
+						?>
+					</div>
 
-	   			<div class="item_comments">
-	   				<?php
-	   					// ------------------ FEEDBACK (COMMENTS/TRACKBACKS) INCLUDED HERE ------------------
-	   					skin_include( '_item_feedback.inc.php', array(
-								'before_section_title' => '<h4 class="tite_comment_status">',
-								'after_section_title'  => '</h4>',
-								'author_link_text'     => 'preferredname',
-								'comment_image_size'   => 'fit-256x256',
-								
-								// Pagination:
-								'pagination' => array(
-									'block_start'           => '<div class="center"><ul class="pagination">',
-									'block_end'             => '</ul></div>',
-									'page_current_template' => '<span>$page_num$</span>',
-									'page_item_before'      => '<li>',
-									'page_item_after'       => '</li>',
-									'page_item_current_before' => '<li class="active">',
-									'page_item_current_after'  => '</li>',
-									'prev_text'             => '<i class="fa fa-angle-double-left"></i>',
-									'next_text'             => '<i class="fa fa-angle-double-right"></i>',
-								),
-							) );
-	   					// Note: You can customize the default item feedback by copying the generic
-	   					// /skins/_item_feedback.inc.php file into the current skin folder.
-	   					// ---------------------- END OF FEEDBACK (COMMENTS/TRACKBACKS) ---------------------
-	   				?>
+					<div class="item_comments">
+						<?php
+						// ------------------ FEEDBACK (COMMENTS/TRACKBACKS) INCLUDED HERE ------------------
+						skin_include( '_item_feedback.inc.php', array(
+							'before_section_title' => '<h4 class="tite_comment_status">',
+							'after_section_title'  => '</h4>',
+							'author_link_text'     => 'preferredname',
+							'comment_image_size'   => 'fit-256x256',
+
+							// Pagination:
+							'pagination' => array(
+								'block_start'           => '<div class="center"><ul class="pagination">',
+								'block_end'             => '</ul></div>',
+								'page_current_template' => '<span>$page_num$</span>',
+								'page_item_before'      => '<li>',
+								'page_item_after'       => '</li>',
+								'page_item_current_before' => '<li class="active">',
+								'page_item_current_after'  => '</li>',
+								'prev_text'             => '<i class="fa fa-angle-double-left"></i>',
+								'next_text'             => '<i class="fa fa-angle-double-right"></i>',
+							),
+						) );
+						// Note: You can customize the default item feedback by copying the generic
+						// /skins/_item_feedback.inc.php file into the current skin folder.
+						// ---------------------- END OF FEEDBACK (COMMENTS/TRACKBACKS) ---------------------
+						?>
 
 						<?php
-							if( evo_version_compare( $app_version, '6.7' ) >= 0 )
-							{  // We are running at least b2evo 6.7, so we can include this file:
-								// ------------------ WORKFLOW PROPERTIES INCLUDED HERE ------------------
-								skin_include( '_item_workflow.inc.php' );
-								// ---------------------- END OF WORKFLOW PROPERTIES ---------------------
-							}
-							?>
+						if( evo_version_compare( $app_version, '6.7' ) >= 0 )
+						{  // We are running at least b2evo 6.7, so we can include this file:
+							// ------------------ WORKFLOW PROPERTIES INCLUDED HERE ------------------
+							skin_include( '_item_workflow.inc.php' );
+							// ---------------------- END OF WORKFLOW PROPERTIES ---------------------
+						}
+						?>
 
-							<?php
-							if( evo_version_compare( $app_version, '6.7' ) >= 0 )
-							{  // We are running at least b2evo 6.7, so we can include this file:
-								// ------------------ META COMMENTS INCLUDED HERE ------------------
-								skin_include( '_item_meta_comments.inc.php', array(
-									'comment_start'  => '<article class="evo_comment evo_comment__meta panel panel-default">',
-									'comment_end'    => '</article>',
-								) );
-								// ---------------------- END OF META COMMENTS ---------------------
-							}
-							?>
-	   			</div><!-- .item_comments -->
+						<?php
+						if( evo_version_compare( $app_version, '6.7' ) >= 0 )
+						{  // We are running at least b2evo 6.7, so we can include this file:
+							// ------------------ META COMMENTS INCLUDED HERE ------------------
+							skin_include( '_item_meta_comments.inc.php', array(
+								'comment_start'  => '<article class="evo_comment evo_comment__meta panel panel-default">',
+								'comment_end'    => '</article>',
+							) );
+							// ---------------------- END OF META COMMENTS ---------------------
+						}
+						?>
+					</div><!-- .item_comments -->
 
-	   		</div><!-- .evo_details -->
-	   	</div><!-- .evo_post_content -->
+				</div><!-- .evo_details -->
+			</div><!-- .evo_post_content -->
 
-	   	<?php
-	   		locale_restore_previous();	// Restore previous locale (Blog locale)
-	   	?>
+			<?php
+			locale_restore_previous();	// Restore previous locale (Blog locale)
+			?>
 
-   	</article><!-- .row -->
-   </main><!-- #main_content -->
-</div><!-- .container -->
+		</article><!-- .row -->
+	</div><!-- .container -->
+</main><!-- #main_content -->
 
 
 <?php
