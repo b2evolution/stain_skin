0a2056489477855122437f17a77a3d62ae60b443
Ariflaw <nurariflaw@gmail.com>
2016-06-15 08:47:15 +0700

    Update Font render

--- _catdir.disp.php
+++ _catdir.disp.php
@@ -95,7 +95,8 @@ $no_content_to_display = true;
         				'image_size'         	     => $Skin->get_setting( 'gallery_thumb_size' ),
         				'image_link_to'      	     => 'single',
         				'image_desc'         	     => '',
-        				'limit'                      => 1,
+						'gallery_image_limit'        => 0, // Don't use images from attached folders.
+						'limit'                      => 1, // Get only first attached image depending on position priority, see param below:
         				'restrict_to_image_position' => 'teaser,aftermore,inline',
         				'get_rendered_attachments'   => false,
         				// Sort the attachments to get firstly "Cover", then "Teaser", and "After more" as last order
